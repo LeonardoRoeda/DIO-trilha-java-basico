ab5c306e3e35d8c13432ce8b73a88c9ed19f7d90
LeonardoRoeda <leonardo99.roeda@gmail.com>
2024-05-04 18:38:46 -0300

    tipos de variaveis e operadores basicos do java

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,18 @@
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+
+        short numeroCurto = 1;
+        short numeroCurto2 = (short) numeroCurto;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
